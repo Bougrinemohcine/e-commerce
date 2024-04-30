5376f6372180b6450c1e19e92a1554eafd49bb14
MohcineBG <mohcinebougrine0@gmail.com>
2024-04-30 23:16:15 +0100

    add admin login and admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('admin',function(){
+    return view('admin.auth.login');
+});
+
+Route::get('admin/dashboard',function(){
+    return view('admin.dashboard');
+});
