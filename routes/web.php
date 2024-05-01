82691f240d14091b2599f3b07e8d337dde05f309
MohcineBG <mohcinebougrine0@gmail.com>
2024-05-02 00:54:32 +0100

    add  role management

--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('login', [AuthController::class, 'login_admin'])->name('login'); // Assuming showLoginForm is the method to show the login form
+Route::get('admin', [AuthController::class, 'login_admin']); // Assuming login_admin is the method for admin login
+Route::post('admin', [AuthController::class, 'auth_login_admin'])->name('auth.login.admin');
+Route::get('admin/logout', [AuthController::class, 'auth_logout'])->name('auth.logout');
 
-    Route::get('admin',[AuthController::class,'login_admin']);
-    Route::post('admin',[AuthController::class,'auth_login_admin'])->name('auth.login.admin');
-    Route::get('admin/logout',[AuthController::class,'auth_logout'])->name('auth.logout');
-
-
-    Route::get('admin/dashboard',function(){
+Route::middleware(['auth'])->group(function () {
+    Route::get('admin/dashboard', function () {
         return view('admin.dashboard');
     });
 
-    Route::get('admin/admin/list',function(){
+    Route::get('admin/admin/list', function () {
         return view('admin.admin.list');
     });
+});
+
